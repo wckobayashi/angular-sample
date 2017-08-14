df9d62fcf03e4ec2179027b2f83c9db466c9cdeb
WEBCREW_JPN\dota.kobayashi <dota.kobayashi@webcrew.co.jp>
2017-08-14 16:17:08 +0900

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Bike } from './bike';
+
+const BIKES: Bike[] = [
+  { id: 1, model: 'CBR250R', manufacturer: 'Honda' },
+  { id: 2, model: 'CBR150R', manufacturer: 'Honda' },
+  { id: 3, model: 'Ninja250R', manufacturer: 'Kawasaki' },
+  { id: 4, model: 'CBR1000R', manufacturer: 'Honda' },
+  { id: 5, model: 'Ninja1000RR', manufacturer: 'Kawasaki' }
+];
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Bikes Showroom';
+  bikes = BIKES;
+  selectedBike: Bike;
+  onSelect(bike: Bike): void {
+    this.selectedBike = bike;
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { BikeComponent } from './bike/bike.component';
+import { BikeInfoComponent } from './bike-info/bike-info.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BikeComponent,
+    BikeInfoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
